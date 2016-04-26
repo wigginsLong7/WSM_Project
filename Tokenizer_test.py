879564a7bf533d22fe957e0b1f5562f1564b979a
wigginsLong7 <peter2_long@163.com>
2016-04-26 23:51:11 +0800

    db

--- Tokenizer_test.py
+++ Tokenizer_test.py
@@ -1,11 +1,7 @@
 from Tokenizer import Tokenizer
 
-<<<<<<< HEAD
 sentences = ["&#1054; &#1068;"]
 
-=======
-sentences = ["&#1054;&#1068;"]
->>>>>>> origin/master
 
 tokenizer = Tokenizer()
 tokenizer.set_stemming(True)
@@ -15,11 +11,10 @@ tokenizer.set_num_del(False)
 for sentence in sentences:
     tokens = sentence.split(" ")
     for token in tokens:
-<<<<<<< HEAD
+
        t = tokenizer.tokenize(token)
        if t != "":
            print(t)
     print("\n")
-=======
-        print(tokenizer.tokenize(token))
->>>>>>> origin/master
+    print(tokenizer.tokenize(token))
+
